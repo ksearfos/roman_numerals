793d0e44ca943297c02d6ace02e7d23bf19d92fb
ksearfos <ksearfos@covermymeds.com>
2016-09-06 14:00:45 -0400

    refactored to green and made it pretty

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -1,52 +1,25 @@
 module RomanNumeral
-  class << self
-    def for(n)
-      str = ''
+  ROMAN_NUMERALS = {
+    1000 => 'M',
+    900 => 'CM',
+    500 => 'D',
+    400 => 'CD',
+    100 => 'C',
+    90 => 'XC',
+    50 => 'L',
+    40 => 'XL',
+    10 => 'X',
+    9 => 'IX',
+    5 => 'V',
+    4 => 'IV',
+    1 => 'I'
+  }
 
-      while n > 0
-        if n >= 1000
-          str << 'M'
-          n -= 1000
-        elsif n == 900
-          str << 'CM'
-          n -= 900
-        elsif n >= 500
-          str << 'D'
-          n -= 500
-        elsif n == 400
-          str << 'CD'
-          n -= 400
-        elsif n >= 100
-          str << 'C'
-          n -= 100
-        elsif n == 90
-          str << 'XC'
-          n -= 90
-        elsif n >= 50
-          str << 'L'
-          n -= 50
-        elsif n == 40
-          str << 'XL'
-          n -= 40
-        elsif n >= 10
-          str << 'X'
-          n -= 10
-        elsif n == 9
-          str << 'IX'
-          n -= 9
-        elsif n >= 5
-          str << 'V'
-          n -= 5
-        elsif n == 4
-          str << 'IV'
-          n -= 4
-        else
-          str << 'I'
-          n -= 1
-        end
-      end
+  def self.for(n)
+    return '' if n == 0
 
-      str
+    ROMAN_NUMERALS.each do |number, roman|
+      return roman + RomanNumeral.for(n - number) if n >= number
     end
   end
 end
